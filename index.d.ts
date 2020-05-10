26c378ee0ce7234bb2be74fef055cf7263e34078
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-05-10 11:34:38 -0300

    Implement type definition for Typescript

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,14 @@
 declare module 'node-config-client'
 
+type LoadOptions = {
+    name: string,
+    profiles: string[],
+    label: string,
+    location: string
+}
+
+interface Config {
+    get(key: string): string
+}
+
+export declare function load(options: LoadOptions): Promise<Config>;
